c1012036f35103838a9bbd7f82c21f440b8f367c
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-07-25 19:50:49 +0300

    Initial Commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Lera Savchenko on 25.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

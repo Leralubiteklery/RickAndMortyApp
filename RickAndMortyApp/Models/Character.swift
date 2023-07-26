4f5b9b93c94b5866372c568503594d857c3a47d4
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-07-26 18:48:09 +0300

    Created model. Re-organized project structure.

---
+++ RickAndMortyApp/Models/Character.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  RickAndMortyApp
+//
+//  Created by Lera Savchenko on 26.07.23.
+//
+
+struct ListOfCharacters {
+    let results: [Character]
+}
+
+struct Character {
+    let name: String
+    let species: String
+    let status: String
+    let imageUrl: String
+}
